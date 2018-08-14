42dad6a6b533aadffd70ee18f2ec8942ff7236b6
George XRjan <Gxrjan@gmail.com>
2018-08-14 15:24:17 +0300

    Added Brand class

--- classes.py
+++ classes.py
@@ -10,18 +10,21 @@ class Product():
 		self.current_price = current_price
 		self.regular_price = regular_price
 
-
 	def __eq__(self, other):
 		return self.product_id == other.product_id and self.product_name == other.product_name
 
-
 	def __hash__(self):
-		return hash(str(self.product_id)+str(self.product_name))
-
+		return hash(str(self.product_id))
 
 	def __str__(self):
 		return self.product_name
 
-
 	def __repr__(self):
 		return self.product_name
+
+
+class Brand():
+	"""Class representing Brand"""
+	def __init__(self, brand_id):
+		self.brand_id = brand_id
+		self.brand_name = brand_name
